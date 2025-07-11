03f37c950dc56fb7593fd68beb09428c83659888
•_• <abhinavaditya035@gmail.com>
2025-07-11 14:34:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,8 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 pyrogram.utils.MIN_CHANNEL_ID = -1002822095762
 SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "@botskingdomschat")
 
+PORT = Config.PORT
+
 class Bot(Client):
     def __init__(self):
         super().__init__(
@@ -35,7 +37,6 @@ class Bot(Client):
         if Config.WEBHOOK:
             app = web.AppRunner(await web_server())
             await app.setup()
-            port = int(os.environ.get("PORT", 8080))
             await web.TCPSite(app, "0.0.0.0", port).start()
         print(f"{me.first_name} Is Started.....✨️")
         uptime_seconds = int(time.time() - self.start_time)
