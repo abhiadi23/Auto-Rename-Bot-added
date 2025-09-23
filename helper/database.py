a5915be604875cbcd9b08daf09a27ff43b6681d4
•_• <abhinavaditya035@gmail.com>
2025-09-23 13:17:55 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -456,67 +456,79 @@ class Database:
             logging.error(f"Error getting media preference for user {id}: {e}")
             return None
 
+    async def ban_user(user_id):
+        await self.banned_users.update_one({"_id": user_id}, {"$set": {"_id": user_id}}, upsert=True)
+ 
+    async def unban_user(user_id):
+        await self.banned_users.delete_one({"_id": user_id})
+
+    async def is_banned(user_id):
+        return await self.banned_users.find_one({"_id": user_id}) is not None
+
+    async def get_banned_users():
+        return self.banned_users.find()
+
     async def get_metadata(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('metadata', "Off") if user else "Off"
+        return user.get('metadata', "Off")
 
     async def set_metadata(self, user_id, metadata):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'metadata': metadata}})
 
     async def get_title(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('title', 'Bots kingdom') if user else 'Bots kingdom'
+        return user.get('title', 'Botskingdom')
 
     async def set_title(self, user_id, title):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'title': title}})
 
     async def get_author(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('author', 'Botskingdoms') if user else 'Botskingdoms'
+        return user.get('author', 'Botskingdom')
 
     async def set_author(self, user_id, author):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'author': author}})
 
     async def get_artist(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('artist', 'Botskingdoms') if user else 'Botskingdoms'
+        return user.get('artist', 'Botskingdom')
 
     async def set_artist(self, user_id, artist):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'artist': artist}})
 
     async def get_audio(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('audio', 'Bots kingdom') if user else 'Bots kingdom'
+        return user.get('audio', 'Botskingdom')
 
     async def set_audio(self, user_id, audio):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'audio': audio}})
 
     async def get_subtitle(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('subtitle', "Botskingdoms") if user else "Botskingdoms"
+        return user.get('subtitle', "Botskingdom")
 
     async def set_subtitle(self, user_id, subtitle):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'subtitle': subtitle}})
 
     async def get_video(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('video', 'Botskingdoms') if user else 'Botskingdoms'
+        return user.get('video', 'Botskingdom')
 
     async def set_video(self, user_id, video):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'video': video}})
 
     async def get_encoded_by(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('encoded_by', "Botskingdoms") if user else "Botskingdoms"
+        return user.get('encoded_by', "Botskingdom")
 
     async def set_encoded_by(self, user_id, encoded_by):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'encoded_by': encoded_by}})
-
+        
     async def get_custom_tag(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('custom_tag', "Botskingdoms") if user else "Botskingdoms"  # Fixed: was 'customtag'
+        return user.get('customtag', "Botskingdom")
 
     async def set_custom_tag(self, user_id, custom_tag):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'custom_tag': custom_tag}})
-
+        
 codeflixbots = Database(Config.DB_URL, Config.DB_NAME)
