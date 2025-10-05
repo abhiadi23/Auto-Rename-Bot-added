223b7466c0b7e093bc998861b664c1f8fbcb8fe1
•_• <abhinavaditya035@gmail.com>
2025-10-05 22:05:37 +0530

    Update helper_func.py

--- plugins/helper_func.py
+++ plugins/helper_func.py
@@ -1,6 +1,4 @@
-#(©)CodeFlix_Bots
-#rohit_1888 on Tg #Dont remove this line
-
+import base64
 import re
 import asyncio
 import time
@@ -12,6 +10,23 @@ from pyrogram.errors import FloodWait
 from helper.database import *
 
 OWNER_ID = Config.OWNER_ID
+
+async def encode(string):
+    string_bytes = string.encode("ascii")
+    base64_bytes = base64.urlsafe_b64encode(string_bytes)
+    base64_string = (base64_bytes.decode("ascii")).strip("=")
+    return base64_string
+
+#===============================================================#
+
+async def decode(base64_string):
+    base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
+    base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
+    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
+    string = string_bytes.decode("ascii")
+    return string
+
+#===============================================================#
 
 #used for cheking if a user is admin ~Owner also treated as admin level
 async def check_admin(filter, client, update):
@@ -70,5 +85,3 @@ async def is_sub(client, user_id, channel_id):
 
 subscribed = filters.create(is_subscribed)
 admin = filters.create(check_admin)
-
-#rohit_1888 on Tg :
